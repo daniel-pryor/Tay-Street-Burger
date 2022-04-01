16defc29c732afefe61f3b73ab0bd05f640fac06
Daniel Pryor <danpryor13@gmail.com>
2022-04-01 15:08:01 +1300

    Mobile Design update

--- main.js
+++ main.js
@@ -3,21 +3,27 @@
 
 function orderNow() {
     // Hide Create Burger Form
-    landingPage = document.querySelector('div[id="landing-page"]');
+    const landingPage = document.querySelector('div[id="landing-page"]');
     if (landingPage.style.display === "none") {
-        landingPage.style.display = "block";
+        landingPage.style.display = "grid";
         } else {
         landingPage.style.display = "none";
         }
 
-    orderForm = document.querySelector('div[id="order-form"]');
-    if (orderForm.style.display === "block") {
+    const orderPage = document.querySelector('div[id="ordering-page"]');
+    if (orderPage.style.display === "grid") {
+        orderPage.style.display = "none";
+    } else {
+        orderPage.style.display = "grid";
+    }
+    const orderForm = document.querySelector('div[id="order-form-page"]');
+    if (orderForm.style.display === "grid") {
         orderForm.style.display = "none";
     } else {
-        orderForm.style.display = "block";
+        orderForm.style.display = "grid";
     }
-    
 }
+
 
 function next () {
     // Get Bun
@@ -50,11 +56,11 @@ function next () {
     document.querySelector('p[id="salads"]').innerHTML = salads.join(", ");
     document.querySelector('p[id="sauces"]').innerHTML = sauces.join(", ");
     document.querySelector('p[id="spice"]').innerHTML = spice;
-
+    
     // Hide Create Burger Form
-    orderForm = document.querySelector('div[id="order-form"]');
+    orderForm = document.querySelector('div[id="order-form-page"]');
     if (orderForm.style.display === "none") {
-        orderForm.style.display = "block";
+        orderForm.style.display = "grid";
       } else {
         orderForm.style.display = "none";
       }
@@ -62,10 +68,10 @@ function next () {
     // Display Order Details
 
     orderDetails = document.querySelector('div[id="order-details"]');
-    if (orderDetails.style.display === "block") {
+    if (orderDetails.style.display === "grid") {
         orderDetails.style.display = "none";
       } else {
-        orderDetails.style.display = "block";
+        orderDetails.style.display = "grid";
       }
 }
 
@@ -90,17 +96,17 @@ function confirmOrder () {
         // Hide Order Details
         orderDetails = document.querySelector('div[id="order-details"]');
         if (orderDetails.style.display === "none" && userName !== "") {
-            orderDetails.style.display = "block";
+            orderDetails.style.display = "grid";
         } else {
             orderDetails.style.display = "none";
         }
 
         // Display Order Confirmation
         orderForm = document.querySelector('div[id="order-confirmation"]');
-        if (orderForm.style.display === "block") {
+        if (orderForm.style.display === "grid") {
             orderForm.style.display = "none";
         } else {
-            orderForm.style.display = "block";
+            orderForm.style.display = "grid";
         }
     } else {
         document.querySelector('p[id="name-alert"]').innerHTML = "Please enter a name for your order"
@@ -118,16 +124,61 @@ function backToCreate () {
     // Hide Order Details
     orderDetails = document.querySelector('div[id="order-details"]');
     if (orderDetails.style.display === "none") {
-        orderDetails.style.display = "block";
+        orderDetails.style.display = "grid";
       } else {
         orderDetails.style.display = "none";
       }
     // Displays Order Form
-    orderForm = document.querySelector('div[id="order-form"]');
-    if (orderForm.style.display === "block") {
+    orderForm = document.querySelector('div[id="order-form-page"]');
+    if (orderForm.style.display === "grid") {
         orderForm.style.display = "none";
     } else {
-        orderForm.style.display = "block";
+        orderForm.style.display = "grid";
     }
 }
 
+
+function total () {
+    // Get Bun
+    let bun = document.querySelector('input[name="bun"]:checked').id;
+    // Get Patty
+    let patty = document.querySelector('input[name="patty"]:checked').id;
+    // Get Spice
+    let spice = document.querySelector('input[name="spice"]:checked').id;
+
+    let bunCost;
+    let pattyCost;
+    let spiceCost;
+
+    if(bun === "gluten-free") {
+        bunCost = 2;
+    } else {
+        bunCost = 0;
+    }
+
+    switch(patty) {
+        case "angus-beef":
+            pattyCost = 10;
+            break;
+        case "impossible-beef":
+            pattyCost = 12;
+            break;
+        case "vegeterian":
+            pattyCost = 8;
+            break;
+        case "crunchy-chicken":
+            pattyCost = 12;
+            break;
+        default:
+    }
+
+    if(spice === "no-spice") {
+        spiceCost = 0;
+    } else {
+        spiceCost = 2;
+    }
+
+    burgerCost = bunCost + pattyCost + spiceCost;
+    document.querySelector('p[id="total"]').innerHTML = "The total is: $" + burgerCost;
+
+}
